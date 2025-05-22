ece4118548abc40bcb90ceb6da1ca3bd03282e78
ekp529 <ekpaupulaire@gmail.com>
2025-05-22 10:20:13 -0400

    finished task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,14 +21,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
   if (books[index]) {
     res.send(JSON.stringify(books[index], null, 2))
   } else {
-    res.status(400).json({message: "Bad ISBN number"})
+    res.send("No book with this ISBN found")
   }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const bookList = Object.values(books).filter(book => book.author === author)
+  if (bookList.length > 0) {
+    res.send(JSON.stringify(bookList, null, 2))
+  } else {
+    res.send("No books found for this author")
+  }
 });
 
 // Get all books based on title
