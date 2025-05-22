3ccf78acb5248121237eb250c126d0f41afbd705
ekp529 <ekpaupulaire@gmail.com>
2025-05-22 10:24:18 -0400

    finished task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,13 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title
+  const bookList = Object.values(books).filter(book => book.title === title)
+  if (bookList.length > 0) {
+    res.send(JSON.stringify(bookList, null, 2))
+  } else {
+    res.send("No books found with this title")
+  }
 });
 
 //  Get book review
